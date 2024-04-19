6526c48b3bbb43a53eb8a5c086b1c1e717df5962
cwalkerr <connor.walker0@proton.me>
2024-04-20 00:40:51 +0100

    add commenting feature

--- api/routes/api_collectionRoutes.js
+++ api/routes/api_collectionRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const collectionController = require("../controllers/api_collectionController.js");
 const cardController = require("../controllers/api_cardController.js");
 const ratingController = require("../controllers/api_ratingController.js");
+const commentController = require("../controllers/api_commentController.js");
 
 /**
  * GET CREATE AND DELETE COLLECTIONS
@@ -35,6 +36,17 @@ router.put("/collections/:id/ratings", ratingController.updateRating);
 router.delete(
   "/collections/:id/ratings/:user_id", // revise this endpoint
   ratingController.deleteRating
+);
+
+/**
+ * COMMENT OPERATIONS
+ */
+
+router.get("/collections/:id/comments", commentController.getAllComments);
+router.post("/collections/:id/comments", commentController.addComment);
+router.delete(
+  "/collections/:collection_id/comments/:comment_id",
+  commentController.deleteComment
 );
 
 module.exports = router;
